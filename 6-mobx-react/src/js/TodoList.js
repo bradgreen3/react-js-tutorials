ab92d14273d278cfa10b11720bc465354afebbff
Brad <bgreen3@uoregon.edu>
2018-07-16 17:57:39 -0500

    Adds completion of tutorial

--- 6-mobx-react/src/js/TodoList.js
+++ 6-mobx-react/src/js/TodoList.js
@@ -1,8 +1,50 @@
 import React from "react"
+import {observer} from 'mobx-react'
 
-
+@observer
 export default class TodoList extends React.Component {
-  render() {
-    return <h1>MobX</h1>
-  }
+    filter(e) {
+        this.props.store.filter = e.target.value
+    }
+
+    createTodo(e) {
+        if (e.which === 13) {
+            this.props.store.createTodo(e.target.value)
+            e.target.value = ''
+        }
+    }
+
+    // different way to bind & pass object?
+    toggleComplete(todo) {
+        todo.complete = !todo.complete
+    }
+
+    render() {
+
+        const {todos, filter, filteredTodos, clearComplete} = this.props.store
+        const todoItems = filteredTodos.map((todo, i) =>
+            <li key={`todo-${i}`}>
+                <input
+                    type='checkbox'
+                    onChange={this.toggleComplete.bind(this, todo)}
+                    value={todo.id}
+                    checked={todo.complete}
+                />
+                {todo.value}
+            </li>
+        )
+
+        return (
+            <div>
+                <h1>todos</h1>
+                <input className='create' onKeyPress={(e) => this.createTodo(e)}/>
+                <input className='filter' value={filter} onChange={(e) => this.filter(e)}/>
+                <ul>{todoItems}</ul>
+                <a href="#"
+                   onClick={this.props.store.clearComplete}>
+                    Clear Complete
+                </a>
+            </div>
+        )
+    }
 }
